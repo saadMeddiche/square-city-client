89a10a1a72c111469a8e552dd4600d246599bc57
SaadMeddiche <saadmeddiche2004201@gmail.com>
2025-12-24 16:24:06 +0100

    simple handshake test

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,24 @@
 alert("Hello and welcome")
+const socket = new WebSocket('ws://localhost:7070', ['saadMeddiche', 'red']);
+
+socket.onopen = () => {
+    console.log('Connected to chat server');
+    socket.send("#HS# SaadMeddiche");
+};
+
+socket.onmessage = (event) => {
+    console.log(`Message received: ${event.data}`);
+};
+
+socket.onerror = (error) => {
+    console.log(`Error: ${error}`)
+}
+
+socket.onclose = (event) => {
+
+    if(event.wasClean) {
+        console.log('Clean close')
+    } else {
+        console.log('Dirty close')
+    }
+};
